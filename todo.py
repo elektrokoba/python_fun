a4ce5e3e85ec0b303f9786715d9bab6f67d58a65
elektrokoba <47393206+elektrokoba@users.noreply.github.com>
2020-06-18 15:38:27 +0200

    Update todo.py
    initial gui

--- todo.py
+++ todo.py
@@ -8,7 +8,28 @@
 # set, get method
 # change color method
 
-import tkinter
+import tkinter as tk
+
+def show_entry_fields():
+    print("item1: %s\nitem2: %s\nitem3: %s" % (e1.get(), e2.get(), e3.get()))
+
+master = tk.Tk()
+tk.Label(master, text="item1").grid(row=0)
+tk.Label(master, text="item2").grid(row=1)
+tk.Label(master, text="item3").grid(row=2)
+
+e1 = tk.Entry(master)
+e2 = tk.Entry(master)
+e3 = tk.Entry(master)
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+e3.grid(row=2, column=1)
+
+tk.Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=tk.W, pady=4)
+tk.Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=tk.W, pady=4)
+
+tk.mainloop()
 
 class MyItems:
     def __init__(self, entry, color, size, isdone):
@@ -31,6 +52,8 @@ class MyItems:
 
 item1 = MyItems("tej", "black", 12, False)
 item2 = MyItems("vaj", "green", 10, True)
+item3 = MyItems("víz", "green", 10, True)
+
 
 print("item1.entryIsDone ",item1.entryIsDone)
 
